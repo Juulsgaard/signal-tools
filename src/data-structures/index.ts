eed75c05c9868f41052d9e8d889dd2283cd0625e
Frederik Sørensen <frederikjs1@hotmail.com>
2024-09-24 14:59:27 +0200

    Export new data structures

---
+++ src/data-structures/index.ts
@@ -0,0 +1,4 @@
+export * from './signal-map';
+export * from './signal-set';
+export * from './signal-queue';
+export * from './signal-stack';
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@
 export * from './async';
 export * from './interop';
 export * from './loading-signal';
-export * from './data-structures/signal-set';
-export * from './data-structures/signal-map';
+export * from './data-structures';
